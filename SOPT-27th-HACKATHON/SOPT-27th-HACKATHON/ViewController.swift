1974be0aae36b290fd1cbe1084ab3ac167128319
i-colours-u <i.colours.u@gmail.com>
2020-11-21 14:59:46 +0900

    [ADD] 최초 파일 생성

---
+++ SOPT-27th-HACKATHON/SOPT-27th-HACKATHON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT-27th-HACKATHON
+//
+//  Created by 송지훈 on 2020/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
