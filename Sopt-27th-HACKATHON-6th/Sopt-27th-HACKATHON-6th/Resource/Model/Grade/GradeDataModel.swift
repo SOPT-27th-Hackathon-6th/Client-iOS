9437a1ca8d5e2429bc1a05a6630c02367aef4061
소영 <sso_0022@naver.com>
2020-11-22 03:13:50 +0900

    랭킹뷰 2번째

---
+++ Sopt-27th-HACKATHON-6th/Sopt-27th-HACKATHON-6th/Resource/Model/Grade/GradeDataModel.swift
@@ -0,0 +1,22 @@
+//
+//  GradeDataModel.swift
+//  Sopt-27th-HACKATHON-6th
+//
+//  Created by soyounglee on 2020/11/22.
+//
+
+import Foundation
+
+struct MaraGradeDataModel {
+    let maraRank: String?
+    let maraName: String?
+    let maraCount: String?
+    let maraLevel: String?
+}
+
+struct GukbabDataModel {
+    let gukbabRank: String?
+    let gukbabName: String?
+    let gukbabCount: String?
+    let gukbabLevel: String?
+}
--- Sopt-27th-HACKATHON-6th/Sopt-27th-HACKATHON-6th/Source/Viewcontroller/Ranking/RankingViewController.swift
+++ Sopt-27th-HACKATHON-6th/Sopt-27th-HACKATHON-6th/Source/Viewcontroller/Ranking/RankingViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class RankingViewController: UIViewController {
-
+    
     
     //MARK:- IBOutlet Part
     /// Label, ColelctionView, TextField, ImageView 등 @IBOutlet 변수들을 선언합니다.  // 변수명 lowerCamelCase 사용
@@ -17,19 +17,24 @@ class RankingViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var selectView: UIView!
     
-
+    @IBOutlet weak var myRankLabel: UILabel!
+    @IBOutlet weak var myNameLabel: UILabel!
+    @IBOutlet weak var myLevelLabel: UILabel!
+    @IBOutlet weak var myCountLabel: UILabel!
+    
     //MARK:- Variable Part
     /// 뷰컨에 필요한 변수들을 선언합니다  // 변수명 lowerCamelCase 사용
     /// ex)  var imageViewList : [UIImageView] = []
     
+    let indexNumber: Int = 0
     
-
+    
     //MARK:- Constraint Part
     /// 스토리보드에 있는 layout 에 대한 @IBOutlet 을 선언합니다. (Height, Leading, Trailing 등등..)  // 변수명 lowerCamelCase 사용
     /// ex) @IBOutlet weak var lastImageBottomConstraint: NSLayoutConstraint!
     
     
-
+    
     //MARK:- Life Cycle Part
     /// 앱의 Life Cycle 부분을 선언합니다
     /// ex) override func viewWillAppear() { }
@@ -43,10 +48,10 @@ class RankingViewController: UIViewController {
         // 셀에 리소스 등록
         tableView.register(rankingTableViewCellNib, forCellReuseIdentifier: "RankingTableViewCell")
         
-        tableView.rowHeight = 100
+        tableView.rowHeight = 72
         tableView.separatorStyle = .none
         tableView.allowsSelection = false
-
+        
         tableView.delegate = self
         tableView.dataSource = self
         
@@ -56,8 +61,24 @@ class RankingViewController: UIViewController {
         selectView.layer.masksToBounds = false
         selectView.layer.shadowOffset = CGSize(width: 0, height: 4) // 반경에 대해서 너무 적용이 되어서 4point 정도 ㅐ림.
         selectView.layer.shadowRadius = 4 // 반경?
-        selectView.layer.shadowOpacity = 0.3 // alpha값입니다.
-
+        selectView.layer.shadowOpacity = 0.1 // alpha값입니다.
+        
+        
+        
+        // bottomView
+        if indexNumber == 0 {
+            myRankLabel.text = "165"
+            myNameLabel.text = "국밥마라 님"
+            myLevelLabel.text = "마라고 1학년"
+            myCountLabel.text = "28 회"
+            
+        }
+        else {
+            myRankLabel.text = "160"
+            myNameLabel.text = "국밥마라 님"
+            myLevelLabel.text = "국밥초 4학년"
+            myCountLabel.text = "11 회"
+        }
         
     }
     
@@ -73,16 +94,16 @@ class RankingViewController: UIViewController {
     ///         myTableView.delegate = self
     ///         myTableView.datasource = self
     ///    }
-
-
+    
+    
     //MARK:- Function Part
     /// 로직을 구현 하는 함수 부분입니다. // 함수명 lowerCamelCase 사용
     /// ex) func tableViewSetting() {
     ///         myTableView.delegate = self
     ///         myTableView.datasource = self
     ///    }
-
-
+    
+    
 }
 
 
@@ -98,24 +119,50 @@ extension RankingViewController: UITableViewDelegate {
 
 extension RankingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        if indexNumber == 0 {
+            return maraDummy.count
+        }
+        else {
+            return gukbabDummy.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RankingTableViewCell", for: indexPath) as! RankingTableViewCell
         
         let row = indexPath.row
-        
-        switch row {
-        case 0:
-            cell.innerView.layer.borderColor = .init(red: 0/255, green: 0/255, blue: 200/255, alpha: 1)
-        case 1:
-            cell.innerView.layer.borderColor = .init(red: 0/255, green: 200/255, blue: 0/255, alpha: 1)
-        case 2:
-            cell.innerView.layer.borderColor = .init(red: 200/255, green: 0/255, blue: 0/255, alpha: 1)
-        default:
-            cell.innerView.layer.borderColor = .init(red: 200/255, green: 200/255, blue: 200/255, alpha: 1)
+        if indexNumber == 0 {
+            cell.rankLabel.text = maraDummy[indexPath.row].maraRank
+            cell.rankNameLabel.text = maraDummy[indexPath.row].maraName! + " 님"
+            cell.rankGradeLabel.text = maraDummy[indexPath.row].maraLevel
+            cell.rankCountLabel.text = maraDummy[indexPath.row].maraCount! + " 회"
+            
+            switch row {
+            case 0...2:
+                cell.rankLabel.textColor = .init(red: 203/255, green: 65/255, blue: 30/255, alpha: 1)
+                cell.innerView.layer.borderColor = .init(red: 203/255, green: 65/255, blue: 30/255, alpha: 1)
+            default:
+                cell.rankLabel.textColor = .init(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
+                cell.innerView.layer.borderColor = .init(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.15)
+            }
         }
+        else {
+            cell.rankLabel.text = gukbabDummy[indexPath.row].gukbabRank
+            cell.rankNameLabel.text = gukbabDummy[indexPath.row].gukbabName! + " 님"
+            cell.rankGradeLabel.text = gukbabDummy[indexPath.row].gukbabLevel
+            cell.rankCountLabel.text = gukbabDummy[indexPath.row].gukbabCount! + " 회"
+            cell.rankLabel.textColor = .init(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
+            switch row {
+            case 0...2:
+                cell.innerView.layer.borderColor = .init(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
+            default:
+                cell.innerView.layer.borderColor = .init(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.15)
+            }
+        }
+        
+        cell.backgroundColor = .clear
+        
+        
         
         return cell
     }
