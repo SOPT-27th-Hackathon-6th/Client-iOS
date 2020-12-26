4e3e28a188fd94e356116e27786d063b1c5b09aa
yujin <glisteneugene@gmail.com>
2020-12-26 21:57:32 +0900

    model

---
+++ Sopt-27th-HACKATHON-6th/Sopt-27th-HACKATHON-6th/Resource/Model/ListModel.swift
@@ -0,0 +1,13 @@
+//
+//  ListModel.swift
+//  Sopt-27th-HACKATHON-6th
+//
+//  Created by ✨EUGENE✨ on 2020/12/26.
+//
+
+import Foundation
+
+struct ListModel {
+    var imageLink, resName, review, updatedAt: String
+    var userIdx: Int
+}
