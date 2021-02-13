40178bde4d9cff7be4445ff7ab6ee309cb47004e
yujin <glisteneugene@gmail.com>
2021-02-13 16:34:41 +0900

    collection view scroll

--- Sopt-27th-HACKATHON-6th/Sopt-27th-HACKATHON-6th/Source/Viewcontroller/Home/CardList/CardViewController.swift
+++ Sopt-27th-HACKATHON-6th/Sopt-27th-HACKATHON-6th/Source/Viewcontroller/Home/CardList/CardViewController.swift
@@ -16,10 +16,11 @@ class CardViewController: UIViewController {
     let horizonInset: CGFloat = 30
     let lineSpacing: CGFloat = 20
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         register()
-        // Do any additional setup after loading the view.
+        setCollectionView()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
@@ -28,9 +29,24 @@ class CardViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
     }
+    func setCollectionView() {
+        let collectionViewLayout: UICollectionViewFlowLayout = {
+            let layout = CustomCVFL()
+            let cellHeight = collectionView.frame.height
+            let cellWidth = collectionView.frame.width - (2 * horizonInset)
+            layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+            layout.minimumLineSpacing = lineSpacing
+            layout.sectionInset = UIEdgeInsets(top: 0, left: horizonInset, bottom: 0, right: horizonInset)
+            layout.scrollDirection = .horizontal
+            return layout
+        }()
+        collectionView.collectionViewLayout = collectionViewLayout
+        
+        collectionView.decelerationRate = .fast
+        collectionView.isPagingEnabled = false
+    }
     func register() {
         collectionView.dataSource = self
-        collectionView.delegate = self
         
         let cardCVNib = UINib(nibName: "CardCollectionViewCell", bundle: nil)
         collectionView.register(cardCVNib, forCellWithReuseIdentifier: "CardCollectionViewCell")
@@ -56,37 +72,18 @@ extension CardViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCollectionViewCell.identifier, for: indexPath) as? CardCollectionViewCell else {
             return UICollectionViewCell()
         }
+        
         cell.type = self.type
         if type == "Mala" {
-            cell.setItem(store: "신룽푸마라탕", date: "2020.10.24", review: "신룽푸는 소세지가 존맛인관계로 최소 8개는 넣어야한다 그게진리다신룽푸는 소세지가 존맛인관계로 최소 8개는 넣어야한다 그게진리다")
+            cell.setItem(store: "신룽푸마라탕", date: "2020.10.24", review: "신룽푸는 소세지가 존맛인관계로 최소 8개는 넣어야한다 그게진리다 신룽푸는 소세지가 존맛인관계로 최소 8개는 넣어야한다 그게진리다")
         } else {
             cell.setItem(store: "국밥국밥", date: "2020.10.24", review: "국밥 웅앵 국밥 웅앵 국밥 웅앵 국밥 웅앵 국밥 웅앵 국밥 웅앵")
         }
         return cell
     }
     
-    
-}
-extension CardViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cellHeight = collectionView.frame.height
-        let cellWidth = collectionView.frame.width - (2 * horizonInset)
-        return CGSize(width: cellWidth, height: cellHeight)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return lineSpacing
-        
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-        
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: horizonInset, bottom: 0, right: horizonInset)
-        
-    }
 }
